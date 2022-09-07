607b4b600671af2f476e954af4df2f24e7cb7e35
GAldereteHero <herogonzalo@gmail.com>
2022-09-07 17:21:00 -0300

    Se agrega componentes CartWidget y ItemListContainer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import 'bulma/css/bulma.min.css';
+import "materialize-css/dist/css/materialize.min.css";
+import "bulma/css/bulma.min.css";
 import "./App.css";
 
 import { NavBar } from "./components/Navbar/Navbar.js";
 import { Footer } from "./components/Footer/Footer.js";
+import { ItemListContainer } from "./components/Containers/ItemListContainer/ItemListContainer.js";
+import { SpacerBlock } from "./components/Containers/SpacerBlock/SpacerBlock.js";
 
 function App() {
+
+  const msj = "Los alimentos órganicos de la tierra a tu mesa";
   return (
     <div className="App">
       <NavBar />
+      <SpacerBlock props={5} />  {/*Props: Specify the space in rem*/}
+      <ItemListContainer greeting={msj} />
       <Footer />
-      
     </div>
   );
 }
